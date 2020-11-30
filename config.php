ce0c6ca3e09f4e3fd40327e6513b176a67a54401
patriotkusuma <patriot.kusumasejati@gmail.co>
2020-11-30 23:06:35 +0700

    Change Server DB

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $server = "us-cdbr-east-02.cleardb.com";
 $username = "b5f22695c614b2";
 $password = "e9b4323f";
-$nameDb = "db_siswa";
+$nameDb = "heroku_5df625ba7788a87";
 
 $db = mysqli_connect($server, $username, $password, $nameDb);
 
